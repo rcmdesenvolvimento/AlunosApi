62727f1979f41919b388da5db6077f9f521a1840
= <=>
2023-11-01 01:26:19 -0300

    Criação da Classe Aluno e as Interfaces e conexão com a base de dados.

---
+++ AlunosApi/Services/Implementation/AlunoServiceImpl.cs
@@ -0,0 +1,40 @@
+﻿using AlunosApi.Models;
+using AlunosApi.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AlunosApi.Services.Implementation
+{
+    public class AlunoServiceImpl : IAlunoService
+    {
+        public Task CreateAluno(Aluno aluno)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAluno(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Aluno> GetAluno(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Aluno>> GetAlunos()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Aluno>> GetAlunosByNome(string nome)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAluno(Aluno aluno)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
